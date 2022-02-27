d1fae8821038238f660eaaa7facf5e35a7035f0f
andreaszico <zicoaritonang@gmail.com>
2022-02-27 21:34:52 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,23 @@ module.exports = {
       primary: ['Poppins'],
       secondary: ['Poly'],
     },
+    screens: {
+      'xs': '520px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     container: {
       padding: {
         DEFAULT: '1rem',
